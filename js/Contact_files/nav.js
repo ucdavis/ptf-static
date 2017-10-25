eb08ae7b0308503a5160a3d13aa670d55c5e86a0
stamstamp <tastamboel@ucdavis.edu>
2017-10-25 15:51:07 -0700

    finished content move

---
+++ js/Contact_files/nav.js
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+  <!-- le Meta -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="subject" content="Plant Research">
+  <meta name="classification" content="Plant Research">
+  <meta name="author" content="">
+  <meta name="designer" content="">
+  <meta name="reply-to" content="">
+  <meta name="theme-color" content="#002855">
+  <link rel="image_src" href="/media/thumbnail.jpg" />
+  <meta property="og:image" content="/media/thumbnail.jpg">
+  <link rel="shortcut icon" href="/media/favicon.ico">
+
+
+  <title>404 Error</title>
+  <meta name="description" content="The Ralph M. Parsons Foundation Plant Transformation Facility was established at the University of California at Davis to provide cost effective plant transformation and plant cell biology services for the plant research community.
+">
+
+
+  <!-- le Apple -->
+  <link rel="apple-touch-icon" href="/media/thumbnail.jpg">
+  <link rel="apple-touch-icon" sizes="76x76" href="/media/76-icon.jpg">
+  <link rel="apple-touch-icon" sizes="120x120" href="/media/120-icon.jpg">
+  <link rel="apple-touch-icon" sizes="152x152" href="/media/156-icon.jpg">
+
+  <!-- le scripts -->
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+
+  <script src="/js/cssbrowserselector.js"></script>
+   <script type="text/javascript" src="/js/nav.js"></script>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+
+
+
+
+  <!-- le CSS -->
+  <link rel="stylesheet" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+  <link href="//cdn.rawgit.com/noelboss/featherlight/1.3.5/release/featherlight.min.css" type="text/css" rel="stylesheet" />
+  <link href="//cdn.rawgit.com/noelboss/featherlight/1.3.5/release/featherlight.gallery.min.css" type="text/css" rel="stylesheet" />
+  <link rel="stylesheet" href="/css/main.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link rel="canonical" href="http://ptf.ucdavis.edu/404.html">
+
+  <link rel="alternate" type="application/rss+xml" title="Plant Transformation Facility" href="http://ptf.ucdavis.edu/feed.xml" />
+
+
+<!-- Begin Jekyll SEO tag v1.4.0 -->
+<title>404 Error - Plant Transformation Facility</title>
+<meta property="og:title" content="404 Error" />
+<meta name="description" content="The Ralph M. Parsons Foundation Plant Transformation Facility was established at the University of California at Davis to provide cost effective plant transformation and plant cell biology services for the plant research community." />
+<meta property="og:description" content="The Ralph M. Parsons Foundation Plant Transformation Facility was established at the University of California at Davis to provide cost effective plant transformation and plant cell biology services for the plant research community." />
+<link rel="canonical" href="http://ptf.ucdavis.edu/404.html" />
+<meta property="og:url" content="http://ptf.ucdavis.edu/404.html" />
+<meta property="og:site_name" content="Plant Transformation Facility" />
+<script type="application/ld+json">
+  {
+    "@context": "http://schema.org",
+    "@type": "WebPage",
+    "headline": "404 Error",
+    "description": "The Ralph M. Parsons Foundation Plant Transformation Facility was established at the University of California at Davis to provide cost effective plant transformation and plant cell biology services for the plant research community.",
+    "url": "http://ptf.ucdavis.edu/404.html"
+  }
+</script>
+<!-- End Jekyll SEO tag -->
+</head>
+
+  <body>
+  <header>
+Header
+        	<nav class="navbar navbar-expand-lg">
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about-us">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/services">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/orders">Orders</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/contact">Contact</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+
+
+</header>
+
+
+
+        <div class="container">
+  <div class="flex-apart separator">
+    <div class="flex-col">
+        <ol class="breadcrumb">
+            <li><a href="/">Home</a></li>
+            <li class="active"><a href="/404.html">404 Error</a></li>
+        </ol>
+    </div>
+</div>
+  <div class="row">
+	<div class="col">
+		<div class="image-wrapper centered">
+		</div>
+		<p>You've discovered a broken link or missing page. <a href="/">Return to the homepage.</a></p>
+	</div>
+</div>
+
+</div>
+
+
+
+    <footer>
+  <div class="t-center foot">
+    <h1>The Ralph M. Parsons Foundation</h1>
+    <p>Plant Transformation Facility <br>University of California<br>One Shields Ave, Davis, CA 95616</p>
+    <p>All contents copyright © 2013</p>
+    <p>please e-mail your comments to: <a class="email-link" href="mailto:dmtricoli@ucdavis.edu" target="_self" title="">David Tricoli</a></p>
+  </div>
+</footer>
+
+
+  </body>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-652647-29"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-652647-29');
+  </script>
+
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+            <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+            <script src="/js/jquery.nivo.slider.pack.js" type="text/javascript"></script>
+            <script type="text/javascript">
+              $(window).load(function() {
+                  $('#slider').nivoSlider({
+                    pauseTime:3000
+                  });
+              });
+            </script>
+
+</html>
